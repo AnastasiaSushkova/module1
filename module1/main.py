bef1c96e89f55c30b558d64c89d7e93a1a49ffd3
AnastasiaSushkova <anastasia.sushkova@knu.ua>
2025-03-28 12:02:35 +0200

    функція записання рядків до файлу

--- module1/main.py
+++ module1/main.py
@@ -6,3 +6,9 @@ def read_file_lines(filename):
         return set()
     with open(filename, 'r', encoding='utf-8') as file:
         return set(line.strip() for line in file if line.strip())
+
+def write_lines_to_file(filename, lines):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in sorted(lines):
+            file.write(line + '\n')
+
